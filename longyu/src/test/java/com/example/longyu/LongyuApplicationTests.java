c34f911287526333dcf70b792639d0b652fc867d
LongWay <1402336840@qq.com>
2024-04-28 01:09:45 +0800

    初始化

---
+++ longyu/src/test/java/com/example/longyu/LongyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.longyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
